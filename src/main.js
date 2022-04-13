429a7e1e0aa89a908252a88ef8074d24962661f9
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2022-04-13 09:20:45 +0800

    開專案

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from "vue-axios";
+import '@/assets/scss/normalize.scss'
+import '@/assets/scss/reset.scss'
+import '@/assets/scss/helper.scss'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, axios)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
